4672c006eeb009629eda7d5238a6b209f3b124e7
Nayana Ramesh <107012@tfcorp.com>
2022-09-06 13:40:25 +0530

    formic-yup files added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Formik } from 'formik';
+import FormHandling from './components/FormHandling';
+import Registration from './components/Registration';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FormHandling /> */}
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
